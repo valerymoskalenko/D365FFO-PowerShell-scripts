ec98e0c1412a0c45a43055b57985a63758141836
Valery Moskalenko <vmoskalenko@gmail.com>
2022-06-03 23:40:42 +0300

    Create Add-D365FOUsers.ps1

---
+++ Add-D365FOUsers.ps1
@@ -0,0 +1,16 @@
+$AccountList = @("john.doe@ciellos.com", "Maria.Ivanova@ciellos.com", "test@ciellos.com") #Add more emails to the list
+$DefaultDataAreaId = 'USMF' #Update default company if it necessary
+
+foreach($account in $AccountList)
+{
+    Write-Host "Working on account email is" $account -ForegroundColor Yellow
+    $UserId = $account.Split("@")[0]
+    $UserName = $UserId.Replace("."," ")
+    $UserName = (Get-Culture).TextInfo.ToTitleCase($UserName)
+
+    Write-Host "   Id  =" $UserId -ForegroundColor Yellow
+    Write-Host "   Name=" $UserName -ForegroundColor Yellow
+
+    Import-D365ExternalUser -Id $UserId -Name $UserName -Email $account -Company $DefaultDataAreaId
+    #Remove-D365User -Email $account
+}
