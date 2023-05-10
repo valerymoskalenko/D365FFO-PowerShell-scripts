6be61a80b04c62816f7c9314a1ee8eed9f16affd
Valery Moskalenko <valery.moskalenko@ciellos.com>
2023-05-10 12:11:33 -0400

    Removed unreadable spaces

--- Add-D365FFODevOpsFeed.ps1
+++ Add-D365FFODevOpsFeed.ps1
@@ -1,48 +1,48 @@
-$DevOpsSessionParameters = @{
-        Instance            = 'https://dev.azure.com/'
-        Collection          = 'Contoso'
-        Project             = 'Dynamics365Dev'
+$DevOpsSessionParameters = @{
+        Instance            = 'https://dev.azure.com/'
+        Collection          = 'Contoso'
+        Project             = 'Dynamics365Dev'
         Account             = 'dmin@contoso.com'
-        PersonalAccessToken = 'taoaaaaaaaaaaaaaaaaPATbbbbbbbbbbbbbbbbbbbbbb5ga'
+        PersonalAccessToken = 'taoaaaaaaaaaaaaaaaaPATbbbbbbbbbbbbbbbbbbbbbb5ga'
     }
 
-$DevOpsFeedName = "D365FO_10.0.33"  #No whitespaces allowed #Please create it manually as Project scoped
-$TempFolder = 'd:\Temp\' + $DevOpsFeedName
-$SASLinks = @( #Copy from LCS SAS links here in any order. Please note starting from 10.0.18, it should 4 links.
-  'https://uswedpl1catalog.blob.core.windows.net/product-ax7productname/84ea2b7f-13d5-4b74-855c-e892fab1d68e/AX7ProductName-12-27-743473ab-8e79-44f7-8d6b-f32ac256d585-84ea2b7f-13d5-4b74-855c-e892fab1d68e?sv=2015-12-11&sr=b&sig=U%2FF2rhmmU6%2BjTaw6DoHRppG1NtN6oU1Ka69X2Pn4ugM%3D&se=2021-04-07T08%3A33%3A15Z&sp=r',
-  'https://uswedpl1catalog.blob.core.windows.net/product-ax7productname/11a66aad-e6c4-4113-ba9f-e207d95d3fa5/AX7ProductName-12-27-743473ab-8e79-44f7-8d6b-f32ac256d585-11a66aad-e6c4-4113-ba9f-e207d95d3fa5?sv=2015-12-11&sr=b&sig=X9dMXJbomxZ%2BiSus5h10cJ3JKGcyurRJa%2Bskh9wI3L8%3D&se=2021-04-07T08%3A33%3A40Z&sp=r',
-  'https://uswedpl1catalog.blob.core.windows.net/product-ax7productname/ac3f8f34-941d-421c-9b02-9bd859b44b4c/AX7ProductName-12-27-743473ab-8e79-44f7-8d6b-f32ac256d585-ac3f8f34-941d-421c-9b02-9bd859b44b4c?sv=2015-12-11&sr=b&sig=y8%2BpBU%2FhFid7PVYOlkJwM2H8uEQAr5DZxruS%2BV%2B4IQU%3D&se=2021-04-07T08%3A34%3A03Z&sp=r',
+$DevOpsFeedName = "D365FO_10.0.33"  #No whitespaces allowed #Please create it manually as Project scoped
+$TempFolder = 'd:\Temp\' + $DevOpsFeedName
+$SASLinks = @( #Copy from LCS SAS links here in any order. Please note starting from 10.0.18, it should 4 links.
+  'https://uswedpl1catalog.blob.core.windows.net/product-ax7productname/84ea2b7f-13d5-4b74-855c-e892fab1d68e/AX7ProductName-12-27-743473ab-8e79-44f7-8d6b-f32ac256d585-84ea2b7f-13d5-4b74-855c-e892fab1d68e?sv=2015-12-11&sr=b&sig=U%2FF2rhmmU6%2BjTaw6DoHRppG1NtN6oU1Ka69X2Pn4ugM%3D&se=2021-04-07T08%3A33%3A15Z&sp=r',
+  'https://uswedpl1catalog.blob.core.windows.net/product-ax7productname/11a66aad-e6c4-4113-ba9f-e207d95d3fa5/AX7ProductName-12-27-743473ab-8e79-44f7-8d6b-f32ac256d585-11a66aad-e6c4-4113-ba9f-e207d95d3fa5?sv=2015-12-11&sr=b&sig=X9dMXJbomxZ%2BiSus5h10cJ3JKGcyurRJa%2Bskh9wI3L8%3D&se=2021-04-07T08%3A33%3A40Z&sp=r',
+  'https://uswedpl1catalog.blob.core.windows.net/product-ax7productname/ac3f8f34-941d-421c-9b02-9bd859b44b4c/AX7ProductName-12-27-743473ab-8e79-44f7-8d6b-f32ac256d585-ac3f8f34-941d-421c-9b02-9bd859b44b4c?sv=2015-12-11&sr=b&sig=y8%2BpBU%2FhFid7PVYOlkJwM2H8uEQAr5DZxruS%2BV%2B4IQU%3D&se=2021-04-07T08%3A34%3A03Z&sp=r',
   ''
 )
 $TFS_FolderWithBuildProject = 'C:\TFS_ALL\Build\BuildProject\BuildProject\'
 $TFS_ProjectFileName = 'BuildProject.rnrproj'
 
-$ErrorActionPreference = "Stop"
-#make sure that TEMP folder is exists
-(New-Item -Path $TempFolder -ItemType Directory -Force).FullName
-#region Installing powershell modules <--
-# This is requried by Find-Module, by doing it beforehand we remove some warning messages
-Write-Host "Installing PowerShell modules d365fo.tools and AzurePipelinesPS" -ForegroundColor Yellow
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+$ErrorActionPreference = "Stop"
+#make sure that TEMP folder is exists
+(New-Item -Path $TempFolder -ItemType Directory -Force).FullName
+#region Installing powershell modules <--
+# This is requried by Find-Module, by doing it beforehand we remove some warning messages
+Write-Host "Installing PowerShell modules d365fo.tools and AzurePipelinesPS" -ForegroundColor Yellow
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; 
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope AllUsers
-#Register-PSRepository -Default -Verbose
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope AllUsers
+#Register-PSRepository -Default -Verbose
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 #Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.6 -Scope CurrentUser -AllowClobber -Repository PSGallery
 
-$modules2Install = @('d365fo.tools')
-foreach ($module in $modules2Install) {
-    Write-Host "..working on module" $module -ForegroundColor Yellow
-    if ($null -eq $(Get-Command -Module $module)) {
-        Write-Host "....installing module" $module -ForegroundColor Gray
-        Install-Module -Name $module -SkipPublisherCheck -Scope AllUsers -Repository PSGallery
-    }
-    else {
-        Write-Host "....updating module" $module -ForegroundColor Gray
-        Update-Module -Name $module
-    }
+$modules2Install = @('d365fo.tools')
+foreach ($module in $modules2Install) {
+    Write-Host "..working on module" $module -ForegroundColor Yellow
+    if ($null -eq $(Get-Command -Module $module)) {
+        Write-Host "....installing module" $module -ForegroundColor Gray
+        Install-Module -Name $module -SkipPublisherCheck -Scope AllUsers -Repository PSGallery
+    }
+    else {
+        Write-Host "....updating module" $module -ForegroundColor Gray
+        Update-Module -Name $module
+    }
 }
-#endregion Installing powershell modules -->
+#endregion Installing powershell modules -->
 Invoke-D365InstallAzCopy -Verbose
 Invoke-D365InstallNuget -Verbose
 Enable-D365Exception -Verbose
@@ -71,42 +71,42 @@ $contentNuGetPackagesConfig = @"
 "@
 $nl = [Environment]::NewLine
 
-[int]$counter = 1
-ForEach($SASLink in $SASLinks)
+[int]$counter = 1
+ForEach($SASLink in $SASLinks)
 {
-    Write-Host "Working on file #" $counter -ForegroundColor Yellow
-    $TempFolderDownload = Join-Path $TempFolder "Download-$counter"
-    New-Item -Path $TempFolderDownload -ItemType Directory -Force
-    $TempFileName = Join-path $TempFolderDownload "File-$counter.zip"
-    
+    Write-Host "Working on file #" $counter -ForegroundColor Yellow
+    $TempFolderDownload = Join-Path $TempFolder "Download-$counter"
+    New-Item -Path $TempFolderDownload -ItemType Directory -Force
+    $TempFileName = Join-path $TempFolderDownload "File-$counter.zip"
+    
 #Download NuGet package from LCS
-    Write-Host "..Downloading file " $TempFileName -ForegroundColor Yellow
-    Invoke-D365AzCopyTransfer -SourceUri $SASLink -DestinationUri $TempFileName -ShowOriginalProgress
-    #make sure that the SAS link is not expired
+    Write-Host "..Downloading file " $TempFileName -ForegroundColor Yellow
+    Invoke-D365AzCopyTransfer -SourceUri $SASLink -DestinationUri $TempFileName -ShowOriginalProgress
+    #make sure that the SAS link is not expired
 
 #Upload NuGet package to Azure DevOps feed
-    Write-Host "..Uploading file " $TempFileName -ForegroundColor Yellow
+    Write-Host "..Uploading file " $TempFileName -ForegroundColor Yellow
     Invoke-D365AzureDevOpsNugetPush -Path $TempFileName -source $DevOpsFeedName -Verbose -EnableException
-    $counter = $counter + 1
+    $counter = $counter + 1
 
 #Get details about NuGet package
-    $ZipStream = [io.compression.zipfile]::OpenRead($TempFileName)
-    $ZipItem = $ZipStream.GetEntry('_rels/.rels')
-    $ItemReader = New-Object System.IO.StreamReader($ZipItem.Open())
-    [xml]$rels = $ItemReader.ReadToEnd()
-    $TargetZipFileName = $($rels.Relationships.Relationship | where {$_.Type -eq 'http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties'}).Target
-    $ZipItem = $ZipStream.GetEntry($TargetZipFileName.Substring(1,$TargetZipFileName.Length-1))
-    $ItemReader = New-Object System.IO.StreamReader($ZipItem.Open())
-    [xml]$target = $ItemReader.ReadToEnd()
+    $ZipStream = [io.compression.zipfile]::OpenRead($TempFileName)
+    $ZipItem = $ZipStream.GetEntry('_rels/.rels')
+    $ItemReader = New-Object System.IO.StreamReader($ZipItem.Open())
+    [xml]$rels = $ItemReader.ReadToEnd()
+    $TargetZipFileName = $($rels.Relationships.Relationship | where {$_.Type -eq 'http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties'}).Target
+    $ZipItem = $ZipStream.GetEntry($TargetZipFileName.Substring(1,$TargetZipFileName.Length-1))
+    $ItemReader = New-Object System.IO.StreamReader($ZipItem.Open())
+    [xml]$target = $ItemReader.ReadToEnd()
     $contentNuGetPackagesConfig = $contentNuGetPackagesConfig + $nl + "  <package id=`"$($target.coreProperties.identifier)`" version=`"$($target.coreProperties.version)`" targetFramework=`"net40`" />"
-    Write-Host "..File is" "<package id=`"$($target.coreProperties.identifier)`" version=`"$($target.coreProperties.version)`"" -ForegroundColor Yellow
+    Write-Host "..File is" "<package id=`"$($target.coreProperties.identifier)`" version=`"$($target.coreProperties.version)`"" -ForegroundColor Yellow
 
 }
 $contentNuGetPackagesConfig = $contentNuGetPackagesConfig + $nl + "</packages>" + $nl
 
 #Creating a new folder in the TSF folder for the Build project/solution
 $TFS_Folder = Join-Path -Path $TFS_FolderWithBuildProject -ChildPath $DevOpsFeedName
-(New-Item -Path $TFS_Folder -ItemType Directory -Force).FullName
+(New-Item -Path $TFS_Folder -ItemType Directory -Force).FullName
 
 $contentNuGetConfig = @"
 <?xml version="1.0" encoding="utf-8"?>
@@ -139,7 +139,7 @@ Write-Host "Updatiing project file" $TFS_ProjectFile -ForegroundColor Yellow
 [xml]$TFS_ProjectContent = Get-Content -Path $TFS_ProjectFile
 if ($null -eq $TFS_ProjectContent.Project.ItemGroup.Folder)
 {
-    Write-Host "There is no folders added to the project. Skipping it. Please add it manually." -ForegroundColor Yellow
+    Write-Host "There is no folders added to the project. Skipping it. Please add it manually." -ForegroundColor Yellow
 }
 else 
 {
